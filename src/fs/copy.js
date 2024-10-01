983f2b974862eb1cd448904b243235d364e93421
solidados <geekbrains.pk@internet.ru>
2024-10-01 22:23:53 +0400

    refactor: update create.js and copy.js - add Object destructure

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -18,30 +18,32 @@ const copy = async () => {
     },
   };
 
+  const { source, destination, errors, copyOptions } = TASK_DATA;
+
   const __dirname = getDirName(import.meta.url);
-  const sourcePath = join(__dirname, TASK_DATA.source.dirName);
-  const destinationPath = join(__dirname, TASK_DATA.destination.dirName);
+  const sourcePath = join(__dirname, source.dirName);
+  const destinationPath = join(__dirname, destination.dirName);
 
   try {
     await access(sourcePath);
     try {
       await access(destinationPath);
-      throw new Error(TASK_DATA.errors.isExist.message);
+      throw new Error(errors.isExist.message);
     } catch (error) {
-      if (error.code !== TASK_DATA.errors.noExist.code) {
-        handleError(error, TASK_DATA.errors.noExist);
+      if (error.code !== errors.noExist.code) {
+        handleError(error, errors.noExist);
       }
     }
 
-    await cp(sourcePath, destinationPath, TASK_DATA.copyOptions);
+    await cp(sourcePath, destinationPath, copyOptions);
     console.log(
-      `>> Success!\nFolder \x1b[34m${TASK_DATA.source.dirName}\x1b[0m now has its own clone \x1b[34m${TASK_DATA.destination.dirName}\x1b[0m`,
+      `>> Success!\nFolder \x1b[34m${source.dirName}\x1b[0m now has its own clone \x1b[34m${destination.dirName}\x1b[0m`,
     );
   } catch (error) {
-    if (error.code === TASK_DATA.errors.noExist.code) {
-      handleError(error, TASK_DATA.errors.noExist);
-    } else if (error.code === TASK_DATA.errors.isExist.code) {
-      handleError(error, TASK_DATA.errors.isExist);
+    if (error.code === errors.noExist.code) {
+      handleError(error, errors.noExist);
+    } else if (error.code === errors.isExist.code) {
+      handleError(error, errors.isExist);
     } else {
       console.error(error.message);
     }
--- src/fs/create.js
+++ src/fs/create.js
@@ -11,20 +11,23 @@ const create = async () => {
     errors: {
       fileExist: { code: "EEXIST", message: "FS operation failed" },
     },
+    options: {
+      flag: "wx",
+      encoding: "utf-8",
+    },
   };
 
+  const { folderName, fileName, content, errors, options } = TASK_DATA;
+
   const __dirname = getDirName(import.meta.url);
-  const pathToFile = join(__dirname, TASK_DATA.folderName, TASK_DATA.fileName);
+  const pathToFile = join(__dirname, folderName, fileName);
   const fileNameWithExt = basename(pathToFile);
 
   try {
-    await writeFile(pathToFile, TASK_DATA.content, {
-      flag: "wx",
-      encoding: "utf-8",
-    });
+    await writeFile(pathToFile, content, options);
     console.log(`>> File \x1b[36m${fileNameWithExt}\x1b[0m was created`);
   } catch (error) {
-    handleError(error, TASK_DATA.errors.fileExist);
+    handleError(error, errors.fileExist);
   }
 };
 
