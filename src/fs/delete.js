113710008dd2d598728094df29c8069100de960c
solidados <geekbrains.pk@internet.ru>
2024-10-01 23:29:33 +0400

    feat: Task src/fs/delete complete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,46 @@
+import { unlink as deleteFile } from "fs/promises";
+import { basename, join } from "node:path";
+import { getDirName } from "../helpers/getDirName.js";
+import handleError from "../helpers/handleError.js";
+import pathExists from "../helpers/pathExists.js";
+
 const remove = async () => {
-    // Write your code here 
+  const TASK_DATA = {
+    destination: { dirName: "files", fileName: "fileToRemove.txt" },
+    errors: {
+      noExist: { code: "ENOENT", message: "FS operation failed" },
+    },
+  };
+
+  const { destination, errors } = TASK_DATA;
+
+  const __dirname = getDirName(import.meta.url);
+  const destinationPath = join(
+    __dirname,
+    destination.dirName,
+    destination.fileName,
+  );
+  const dstFileNameWithExt = basename(destinationPath);
+
+  try {
+    const destinationExist = await pathExists(destinationPath);
+    !destinationExist &&
+      handleError(new Error(errors.noExist.message), errors.noExist);
+
+    await deleteFile(destinationPath);
+    console.log(
+      `>> Success!\nFile \x1b[36m${dstFileNameWithExt}\x1b[0m was removed`,
+    );
+  } catch (error) {
+    if (error.message === errors.noExist.message) {
+      handleError(error, errors.noExist);
+    } else {
+      handleError(error, { code: "UNKNOWN", message: "FS operation failed" });
+    }
+  }
 };
 
-await remove();
+await remove().catch((error) => {
+  console.error(error.message);
+  process.exit(1);
+});
