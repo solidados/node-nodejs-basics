4e2d62fcd2d78cdf7a5324670b31eecb92134eb3
solidados <geekbrains.pk@internet.ru>
2024-10-02 00:10:57 +0400

    feat: Task src/fs/read complete

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,51 @@
+import { readFile } from "fs/promises";
+import { basename, join } from "node:path";
+import { getDirName } from "../helpers/getDirName.js";
+import handleError from "../helpers/handleError.js";
+import pathExists from "../helpers/pathExists.js";
+
 const read = async () => {
-    // Write your code here 
+  const TASK_DATA = {
+    destination: { dirName: "files", fileName: "fileToRead.txt" },
+    errors: {
+      noExist: { code: "ENOENT", message: "FS operation failed" },
+    },
+    options: {
+      encoding: "utf-8",
+      flag: "r",
+    },
+  };
+
+  const { destination, errors, options } = TASK_DATA;
+
+  const __dirname = getDirName(import.meta.url);
+  const destinationPath = join(
+    __dirname,
+    destination.dirName,
+    destination.fileName,
+  );
+  const fileNameWithExt = basename(destinationPath);
+
+  try {
+    const destinationExist = await pathExists(destinationPath);
+    !destinationExist &&
+      handleError(new Error(errors.noExist.message), errors.noExist);
+
+    const fileInfo = await readFile(destinationPath, options);
+
+    console.log(
+      `>> Success!\nFile \x1b[34m${fileNameWithExt}/\x1b[0m contains:\n\x1b[32m${fileInfo}\x1b[0m`,
+    );
+  } catch (error) {
+    if (error.message === errors.noExist.message) {
+      handleError(error, errors.noExist);
+    } else {
+      handleError(error, { code: "UNKNOWN", message: "FS operation failed" });
+    }
+  }
 };
 
-await read();
+await read().catch((error) => {
+  console.error(error.message);
+  process.exit(1);
+});
