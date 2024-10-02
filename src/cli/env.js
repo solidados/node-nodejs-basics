058e349e803b48f472a884fde16c17511875e74e
solidados <geekbrains.pk@internet.ru>
2024-10-02 11:36:58 +0400

    refactor: Task src/cli/env update with colors

--- src/cli/env.js
+++ src/cli/env.js
@@ -2,7 +2,7 @@ const parseEnv = () => {
   const prefix = "RSS_";
   const envVariables = Object.keys(process.env)
     .filter((key) => key.startsWith(prefix))
-    .map((key, value) => `${key}=${value}`)
+    .map((key, value) => `\x1b[32m${key}=${value}\x1b[0m`)
     .join("; ");
   console.log(envVariables);
 };
