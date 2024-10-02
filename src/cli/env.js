3e1b7f6b7a4de5212c6316fea15a59195d5d0e4d
solidados <geekbrains.pk@internet.ru>
2024-10-02 11:34:25 +0400

    feat: Task src/cli/env complete

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,10 @@
 const parseEnv = () => {
-    // Write your code here 
+  const prefix = "RSS_";
+  const envVariables = Object.keys(process.env)
+    .filter((key) => key.startsWith(prefix))
+    .map((key, value) => `${key}=${value}`)
+    .join("; ");
+  console.log(envVariables);
 };
 
 parseEnv();
