6ebc118ff802e43c79cfb08e92934c5afb0e457b
solidados <geekbrains.pk@internet.ru>
2024-10-02 13:37:31 +0400

    feat: Task src/module: migrate to ESM before createServer

--- src/modules/esm.mjs
+++ src/modules/esm.mjs
@@ -1,24 +1,36 @@
-const path = require('path');
-const { release, version } = require('os');
-const { createServer: createServerHttp } = require('http');
-require('./files/c');
+import {join, sep} from 'node:path';
+// import { readFileSync } from "node:fs";
+import { readFile } from "node:fs/promises";
+import { release, version } from 'node:os'
+import { createServer as createServerHttp } from 'node:http';
+
+import { getDirName, getFileName } from "../helpers/getDirName.js";
+
+import './files/c.js';
+
+const __dirname = getDirName(import.meta.url)
+const __filename = getFileName(import.meta.url)
+
+const pathToFileA = join(__dirname, './files/a.json')
+const pathToFileB = join(__dirname, './files/b.json')
+
+const options = {encoding: 'utf-8'}
 
 const random = Math.random();
-
 let unknownObject;
 
 if (random > 0.5) {
-    unknownObject = require('./files/a.json');
+    unknownObject = JSON.parse(await readFile(pathToFileA, options))
 } else {
-    unknownObject = require('./files/b.json');
+    unknownObject = JSON.parse(await readFile(pathToFileB, options))
 }
 
 console.log(`Release ${release()}`);
 console.log(`Version ${version()}`);
-console.log(`Path segment separator is "${path.sep}"`);
+console.log(`Path segment separator is "\x1b[32m${sep}\x1b[0m"`);
 
-console.log(`Path to current file is ${__filename}`);
-console.log(`Path to current directory is ${__dirname}`);
+console.log(`Path to current file is \x1b[32m${__filename}\x1b[0m`);
+console.log(`Path to current directory is \x1b[32m${__dirname}\x1b[0m`);
 
 const myServer = createServerHttp((_, res) => {
     res.end('Request accepted');
