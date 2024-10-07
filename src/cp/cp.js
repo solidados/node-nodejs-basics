f2d2580e33c5bef0681a2f1ba66f22e15357044f
solidados <geekbrains.pk@internet.ru>
2024-10-07 16:52:40 +0400

    feat: Task src/zip/cp complete

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,37 @@
+import { spawn } from "node:child_process";
+import { join } from "node:path";
+import process from "node:process";
+import { getDirName } from "../helpers/getDirName.js";
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const __dirname = getDirName(import.meta.url);
+  const pathToScript = join(__dirname, "files", "script.js");
+  const errors = {
+    processError: {},
+  };
+
+  const childProcess = spawn("node", [pathToScript, ...args], {
+    stdio: ["pipe", "pipe", "inherit", "ipc"],
+  });
+
+  childProcess.send({ greeting: "Hello! Send me some nice words bellow: " });
+
+  process.stdin.pipe(childProcess.stdin);
+  childProcess.stdout.pipe(process.stdout);
+  childProcess.on("exit", (exitCode) => {
+    if (exitCode !== 0) {
+      console.log(`Child process exit with code \x1b[31m${exitCode}\x1b[0m`);
+    } else {
+      console.log(`\x1b[33mChild process completed successfully\x1b[0m`);
+    }
+  });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+await spawnChildProcess([
+  "--experimental-permission",
+  "--allow-fs-read=*",
+]).catch((error) => {
+  console.error(error.message);
+  process.exit(1);
+});
